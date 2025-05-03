69bbc916d5844fd5958bda798394c9ab8fcea6ae
dinesh15-hub <dineshchandrareddy75@gmail.com>
2025-05-03 12:17:38 +0530

    3

--- my-personal-website/js/script.js
+++
@@ -1,8 +0,0 @@
-// Function to scroll to the projects section when the button is clicked
-function scrollToProjects() {
-  const projectsSection = document.getElementById('projects');
-  window.scrollTo({
-    top: projectsSection.offsetTop,
-    behavior: 'smooth'
-  });
-}
